dc37b1131d17290b3ce0530d65d85a80694ac2f8
Meraj Bighamian <merajbighamian@gmail.com>
2023-02-25 23:26:44 +0330

    ]

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input_1 = Console.ReadLine();
+            string input_2 = Console.ReadLine();   
+            int num_1 = Convert.ToInt32(input_1);
+            int num_2 = Convert.ToInt32(input_2);
+            Console.WriteLine(num_1 + num_2);
+
+        }
+    }
+}
