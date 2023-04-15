a6a06162763d7cad71b3c6eaba412f26a4e9e2ca
Meraj Bighamian <merajbighamian@gmail.com>
2023-04-15 23:36:33 +0430

    add project math fromula and add result of calc box volume project

---
+++ calc_math_fromula/calc_math_fromula/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Project
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            Calc(x, y);
+
+        }
+
+        private static void Calc(int a, int b)
+        {
+            double result = Math.Sqrt(Math.Abs(a)+Math.Pow(b,2));
+            Console.WriteLine(result);
+        }
+    }
+}
