52907d37c83c7c73dc07df44d841cec19f0d7300
Meraj Bighamian <merajbighamian@gmail.com>
2023-04-15 23:25:46 +0430

    add project calc box volume

---
+++ calc_box_volume/calc_box_volume/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Project
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int l = Convert.ToInt32(Console.ReadLine());
+            int h = Convert.ToInt32(Console.ReadLine());
+            int w = Convert.ToInt32(Console.ReadLine());
+
+            double show = Calc(l, h, w);
+            Console.WriteLine(show);
+
+        }
+
+        private static double Calc(int num1, int num2 ,int num3)
+        {
+            double result = num1 * num2 * num3;
+            return result;
+        }
+    }
+}
