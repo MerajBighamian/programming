6df1f791ef1cc6ba18d0e629e87be7f03d707adf
Meraj Bighamian <merajbighamian@gmail.com>
2023-04-15 23:49:02 +0430

    add project math fromula 2

---
+++ math_fromula2/math_fromula2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Project
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Calc(x);
+
+
+        }
+
+        private static void Calc(int x)
+        {
+            double result = Math.Pow(x,2)+(2*x)-4;
+            Console.WriteLine(result);
+        }
+    }
+}
