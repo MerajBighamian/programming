df14b7bc271a181d5fd87cc20ce888bf87059927
Meraj Bighamian <merajbighamian@gmail.com>
2023-04-16 00:13:41 +0430

    add project calc avg of student

---
+++ clac_avg_of_student/clac_avg_of_student/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Project
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double mid = Convert.ToDouble(Console.ReadLine());
+            double final = Convert.ToDouble(Console.ReadLine());
+
+
+            double result = Calc(mid, final);
+            if(result >= 10)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Faild");
+            }
+
+        }
+
+        private static double Calc(double Mid, double Final)
+        {
+            double result = (Mid * 0.35) + (Final * 0.65);
+            return result;
+        }
+    }
+}
